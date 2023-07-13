36eacb885ee8191487a94a928ac1a882a2eafe36
Andrew Creekmore <andrew.creekmore@me.com>
2023-07-13 15:46:10 -0500

    added ModeOptionMenu drop-down; added result + most recent operation output labels;

--- src/calculator.py
+++ src/calculator.py
@@ -1,10 +1,10 @@
 """
 calculator.py: 
 - primary app logic / entry-point
-- creates instance of CalcApp and loops
+- creates instance of CalcApp, which runs main loop
 """
 
-import enum
+from enum import Enum
 import customtkinter as ctk
 import darkdetect
 try: # windows only
@@ -14,14 +14,14 @@ except:
 from settings import *
 
 
-class CalcMode(enum.IntEnum):
-    """ modes of operation """
-    Standard = 1
-    Programming = 2
+class CalcMode(Enum):
+    """ Represents possible modes of CalcApp operation. """
+    CM_STANDARD = "Standard"
+    CM_PROGRAMMING = "Programming"
 
 
 class CalcApp(ctk.CTk):
-    """ main / core application class """
+    """ Main / core application class. """
     
     def __init__(self, isDark):
         """ """
@@ -40,13 +40,25 @@ class CalcApp(ctk.CTk):
         self.changeTitleBarColor(isDark) # change title bar to match rest of window
 
         # default to Standard operating mode
-        self.currentMode = CalcMode(1)
+        self.currentMode = CalcMode('Standard')
+        
+        # setup grid layout
+        self.rowconfigure(list(range(STD_MAIN_ROWS)), weight = 1, uniform= 'a')
+        self.columnconfigure(list(range(STD_MAIN_COLUMNS)), weight = 1, uniform= 'a')
+
+        # data
+        self.resultString = ctk.StringVar(value = '0')
+        self.lastOperationString = ctk.StringVar(value = '')
+
+        # setup widgets
+        self.createWidgets()
 
         # run
         self.mainloop()
 
+
     def changeTitleBarColor(self, isDark):
-        """ """
+        """ If on Windows platform, changes app's title bar color to match rest of window. """
         try: # windows only
             HWND = windll.user32.GetParent(self.winfo_id()) # get current window
             DWMA_ATTRIBUTE = 35 # target color attribute of window's title bar
@@ -54,6 +66,56 @@ class CalcApp(ctk.CTk):
             windll.dwmapi.DwmSetWindowAttribute(HWND, DWMA_ATTRIBUTE, byref(c_int(TITLE_BAR_COLOR)), sizeof(c_int)) # set attribute
         except:
             pass
+
+    def createWidgets(self):
+        """ Initializes all CalcApp widgets: ModeOptionMenu, OutputLabel(s). """
+
+        # create widget font
+        mainWidgetFont = ctk.CTkFont(family = FONT, size = STD_NORMAL_FONT_SIZE)
+        resultFont = ctk.CTkFont(family = FONT, size = STD_OUTPUT_FONT_SIZE)
+
+        # create mode menu
+        ModeOptionMenu(self)
+        DebugCheckModeButton(self)
+
+        # create output labels
+        OutputLabel(self, 0, 'se', mainWidgetFont, self.lastOperationString) # last entered operation
+        OutputLabel(self, 1, 'e', resultFont, self.resultString) # result
+
+
+class ModeOptionMenu(ctk.CTkOptionMenu):
+    """ Drop-down menu allowing CalcApp operating modes (i.e., Standard, Programming). """
+
+    def __init__(self, parent):
+
+        super().__init__(master = parent, width = 100, fg_color = (WHITE, BLACK), button_color = (WHITE, BLACK), button_hover_color = GRAY,
+                         text_color = (BLACK, WHITE), font = ctk.CTkFont(family = FONT, size = MODE_SWITCH_FONT_SIZE),
+                         values = [e.value for e in CalcMode], command = self.modeOptionMenuCallback)
+        self.place(relx = 0.02, rely = 0.02, anchor = 'nw') # place in top-right; northeast anchor
+
+    def modeOptionMenuCallback(self, selection):
+        """ Sets CalcApp's currentMode variable to be equivalent to the selected string menu option. """
+        self.master.currentMode = CalcMode(selection)
+
+
+class DebugCheckModeButton(ctk.CTkButton):
+    """ DEBUG ONLY: returns CalcApp's currentMode. """
+
+    def __init__(self, parent):
+        """ """
+        super().__init__(master = parent, text = "DEBUG: get currentMode", command = self.printCurrentMode)
+        self.grid(column = 0, columnspan = 4, row = 4)
+
+    def printCurrentMode(self):
+        print(self.master.currentMode.value)
+
+
+class OutputLabel(ctk.CTkLabel):
+    """ Label representing calculator output: last performed operation, operation result, etc. """
+    def __init__(self, parent, row, anchor, font, stringVar):
+
+        super().__init__(master = parent, font = font, textvariable = stringVar)
+        self.grid(column = 0, columnspan = 4, row = row, sticky = anchor, padx = 10)
 
 
 if __name__ == '__main__':
--- src/settings.py
+++ src/settings.py
@@ -6,14 +6,46 @@ settings.py: formatting + positional data
 
 # basic layout sizing
 WINDOW_SIZE = (400, 700)
+STD_MAIN_ROWS = 7
+STD_MAIN_COLUMNS = 4
+PROG_MAIN_ROWS = 7
+PROG_MAIN_COLUMNS = 4
 
 # text attributes
 FONT = 'Helvetica'
+MODE_SWITCH_FONT_SIZE = 14
+STD_OUTPUT_FONT_SIZE = 70
+STD_NORMAL_FONT_SIZE = 32
+PROG_OUTPUT_FONT_SIZE = 70
+PROG_NORMAL_FONT_SIZE = 32
 
 # color definitions
 BLACK = '#000000'
 WHITE = '#EEEEEE'
+LIGHT_GRAY = '#E8E8E8'
+GRAY = '#D9D9D9'
 TITLE_BAR_HEX_COLORS = {
     'dark': 0x00000000,
     'light': 0x00EEEEEE
+}
+
+COLORS = {
+    'lightGray': {'fg': ('#505050', '#D4D4D2'), 'hover': ('#686868', '#EFEFED'), 'text': ('white', 'black')},
+    'darkGray': {'fg': ('#D4D4D2', '#505050'), 'hover': ('#EFEFED', '#686868'), 'text': ('black', 'white')},        
+    'orange': {'fg': '#FF9500', 'hover': '#FFB143', 'text': ('black', 'white')},    
+    'orangeHighlight': {'fg': 'white', 'hover': 'white', 'text': ('black', 'FF9500')}       
+}
+
+NUMBER_POSITIONS = {
+    '.': {'col': 2, 'row': 6, 'span': 1},
+    0: {'col': 0, 'row': 6, 'span': 2},
+    1: {'col': 0, 'row': 5, 'span': 1},
+    2: {'col': 1, 'row': 5, 'span': 1},
+    3: {'col': 2, 'row': 5, 'span': 1},
+    4: {'col': 0, 'row': 4, 'span': 1},
+    5: {'col': 1, 'row': 4, 'span': 1},
+    6: {'col': 2, 'row': 4, 'span': 1},
+    7: {'col': 0, 'row': 3, 'span': 1},
+    8: {'col': 1, 'row': 3, 'span': 1},
+    9: {'col': 2, 'row': 3, 'span': 1},
 }
